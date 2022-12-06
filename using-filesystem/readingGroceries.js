d04dd94a8e1de3322c3f36b872bc9fc021d3d3b5
biangomes <biangomes0707@gmail.com>
2022-12-06 15:12:25 -0500

    Ainda não está lendo csv

--- using-filesystem/readingGroceries.js
+++ using-filesystem/readingGroceries.js
@@ -2,5 +2,12 @@ const http = require('http');
 const fs = require('fs').promises;
 
 async function readingCsv() {
-
+    try {
+        const data = await readFile(filePath, { encoding: 'utf8' });
+        console.log(data);
+    } catch (error) {
+        return `Error opening file: ${error.message}`;
+    }
 }
+
+readingCsv('using-filesystem/groceries.csv');
